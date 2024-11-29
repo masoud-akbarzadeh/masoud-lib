ced99abfb6c9667e793eb93372988e96b10a74f8
Masoud Akbarzadeh <masoud.akbarzadeh.edu@gmail.com>
2024-11-28 23:38:48 -0700

    testing add another function to see how the system is working

--- masoud_lib/aerosol.py
+++ masoud_lib/aerosol.py
@@ -1,4 +1,6 @@
-def c_cunningham(d_p, lamda=65E-9):
+def c_cunningham(d_p # particle diameter in meters
+                 ,lamda=65E-9 # mean free path of the gas in meters
+                 ):
     # defining the function
     # Cunningham correction factor
     # Dp is the particle diameter in meters
@@ -6,3 +8,15 @@ def c_cunningham(d_p, lamda=65E-9):
     # C is the Cunningham correction factor
     kn = 2 * lamda / d_p
     return 1 + kn * (1.257 + 0.4 * np.exp(-1.1 / kn))
+
+
+def mean_free_path(temperature: float, pressure: float):
+    # mean free path of air calculator
+    # T is the temperature in Kelvin
+    # P is the pressure in Pascals
+    # lamda is the mean free path of the gas in meters
+    R = 8.314  # J/(mol K) gas constant
+    M = 0.0289647  # kg/mol molar mass of air
+    air = Fluid(FluidsList.Air).with_state(Input.pressure(pressure), Input.temperature(temperature - 273.15))
+    viscosity = air.dynamic_viscosity  # Pa s dynamic viscosity
+    return 2 * viscosity / (pressure * np.sqrt(8 * M / (np.pi * R * temperature)))
